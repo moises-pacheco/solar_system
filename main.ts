35341fee56387a4ba62c03407b631d71be328312
moises-pacheco <zmoises.pacheco@gmail.com>
2026-05-02 14:06:31 +0200

    Posición cámara
    Cambios en la posición inicial de la cámara

--- main.ts
+++ main.ts
@@ -10,7 +10,7 @@ import { Stars } from './createStars';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1 , 1000);
-camera.position.set(0,0,30);
+camera.position.set(0,25,90);
 const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(Math.min(devicePixelRatio,2));
